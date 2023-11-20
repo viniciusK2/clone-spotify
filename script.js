4509a7a3960fa55270b24eb9971637727f595ce8
Vinicius Lima <viniciuslimadacruz6@gmail.com>
2023-11-19 23:07:22 -0300

    Correção do navbar para resoluções diferentes

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let navbar = document.getElementById('navbar')
 let footer = document.getElementById('footer')
 
-let topoFooter = 160;
+let topoFooter = 210;
 
 window.addEventListener('scroll', function(){
     const foot = footer.getBoundingClientRect();
-    
-    if(foot.top <= topoFooter){
+
+    if(foot.top <= 110){
         navbar.style.display = 'none';
+    }
+    else{
+        navbar.style.display = 'flex';
     } 
+
+    if(matchMedia('(max-width: 500px)').matches)
+        if(foot.top <= topoFooter){
+            navbar.style.display = 'none';
+        } 
     else {
         navbar.style.display = 'flex';
     }
